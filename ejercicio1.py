ccc2a45b67f6aec532d13cf633870421b7d9fecf
Uriel Medina <urimeba511@gmail.com>
2020-02-04 19:26:31 -0600

    *added* Ejercicios1 File
    Added the Python File worked on the first Alfas Class

---
+++ ejercicio1.py
@@ -0,0 +1,56 @@
+# TIPOS DE DATOS
+# numero = 0
+# decimal = 1.1
+# string = "Hola!"
+# booleano = True
+# listas = [1,2,3,4,5]
+# tuplas = (1,2)
+# diccionarios = {"Llave":"Valor"}
+
+# IMPRESION DE VARIABLES
+# print(numero, decimal, string, booleano, listas, tuplas, diccionarios)
+
+# ENTRADA DE DATOS
+# nombre = input("Ingresa tu nombre: ")
+# edad = int(input("Ingresa tu edad: "))
+# print("Hola " + nombre + ". Tu edad es: " + str(edad))
+# print("Tu edad en diez años sera " + str((edad + 10)))
+
+# VARIABLES GLOBALES Y LOCALES
+# x = "global"
+# def miFuncion():
+#     x = "local"
+#     print("Hola, soy una variable " + x)
+# miFuncion()
+# print("Hola, soy una variable " + x)
+
+# ESTRUCTURAS DE CONTROL
+# if(True or 1==0):
+#     print("Hola! Soy True")
+# elif(True and 1==2):
+#     print("Estoy en un elif")
+# else:
+#     print("Hola, soy else")
+
+# ESTRUCTURAS DE DATO
+# año = 2000
+# while(año<2020):
+#     print("Año: " + str(año))
+#     año+=1
+
+# string = "Hola mundo"
+# for letra in string: 
+#     print(letra)
+
+
+# listas = [[1,2,3], [4,5,6], [7,8,9]]
+
+# string = ""
+# for lista in listas:
+#     # print(lista)
+
+#     for numero in lista:
+#         # print(numero)
+#         string += str(numero) + " "
+
+# print(string)
