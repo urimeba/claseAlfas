352bfc03637a49c8e2081e5a83cc641449be43f4
Uriel <urimeba511@gmail.com>
2020-02-05 23:22:24 -0600

    *added* Exercises #1 File
    Added the Python File where the students can get some more extra information, plus logic, in Python, developing certain knowledge like lists, dictionaries, for cycles, if...else, among others, including first approach to Git

---
+++ ejercicios1.py
@@ -0,0 +1,36 @@
+"""
+EJERCICIO #1
+Crea un programa que pida el nombre y la edad del usuario.
+El programa debera imprimir en que año el usuario cumplira 100 años
+"""
+
+"""
+EJERCICIO #2
+Crea un programa que pida un numero entero positivo y le haga saber al usuario si es par o primo
+(PISTA: investiga el operador modulo, denotado como % en muchos lenguajes de programacion)
+"""
+
+"""
+EJERCICIO #3
+Tome estas listas: 
+a = [1, 7, 22, 90, 32, 2, 92, 85, 12]
+b = [1, 5, 77, 22, 90, 25, 83, 100, 02, 21, 90]
+y crea un programa que cree una nueva lista (o array)
+que contenga los elementos que NO se encuentran repetidos en ninguna de las dos lista anteriores
+y se imprima la lista completa (tambien se puede imprimir cada elemento de la lista)
+"""
+
+"""
+EJERCICIO #4
+Crea un programa que le pida al usuario una palabra y muestre si esa palabra es un palindromo
+NOTA: un palindromo es una palabra que se puede escribir al derecho y al reves de la misma manera.
+(Por ejemplo: ana, ala, oso)
+(EXTRA: intenta hacerlo en 4 lineas de codigo solamente (investiga IF TERNARIO en Python))
+"""
+
+"""
+EJERCICIO #5
+Crea un programa con un diccionario que contenga de llave un nombre
+y de valor el cumpleaños de esta persona.
+El programa pedira el nombre de la persona y se debera imprimir la fecha de su cumpleaños
+"""
