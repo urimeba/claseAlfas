31037e673a72106a8ef50bd0762b51f04f76664e
Uriel <urimeba511@gmail.com>
2020-02-10 23:22:48 -0600

    *feat* Classes in Python
    Added ejercicio3.py, where the student can look how a class is composed, their sintaxis, the uses of it, and more

---
+++ ejercicios3.py
@@ -0,0 +1,35 @@
+"""
+Estructura de una funcion en Python:
+def nombre_funcion(argumento1, argumento2, n_argumento):
+    realiza_alguna_operacion
+    o_imprime_algo
+    o regresa algun valor con
+    return argumento1 + argumento2
+"""
+
+""" ¡NOTA LA IDENTACION! """
+def saludo(nombre):
+    return "Hola " + nombre + "!! Bienvenido!"
+
+saludo("Uriel")
+
+
+
+
+
+def suma(numero1, numero2):
+    print( numero1 + numero2)
+
+suma(1,2)
+
+
+
+
+
+def resta(a, b):
+    return a - b
+
+resta(5,1)
+resta(b=5, a=1)
+
+
